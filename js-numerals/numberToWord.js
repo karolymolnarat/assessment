9b2a1eeb4c8724b73dfbea0a2b0edc398d27bee7
karolymolnarat <karolymolnarat@gmail.com>
2021-04-22 11:50:01 +0200

    conditon for one and two-digit numbers till 20

--- js-numerals/numberToWord.js
+++ js-numerals/numberToWord.js
@@ -17,6 +17,24 @@ function numToWord(number) {
   if (number < 20) {
     return digitToNineteen[ number ];
   }
+  if (numberString.length === 2) {
+    return tenthDigit[ numberString[ 0 ] ] + '-' + digitToNineteen[ numberString[ 1 ] ];
+  }
+  if (numberString.length === 3) {
+    return digitToNineteen[ numberString[ 0 ] ] + ' ' + 'hundred' + ' and ' + tenthDigit[ numberString[ 1 ] ] + '-' + digitToNineteen[ numberString[ 2 ] ];
+  }
+  if (numberString.length === 4) {
+    return digitToNineteen[ numberString[ 0 ] ] + ' ' + 'thousand' + ' ' + digitToNineteen[ numberString[ 1 ] ] + ' ' + 'hundred' + ' and ' + tenthDigit[ numberString[ 2 ] ] + '-' + digitToNineteen[ numberString[ 3 ] ];
+  }
+  if (numberString.length === 5) { 
+    if (numberString[ 0 ] === 1) {
+      const twoFirst = 10 + Number(numberString[ 1 ]);
+      return digitToNineteen[ twoFirst ] + ' ' + 'thousand' + ' ' + digitToNineteen[ numberString[ 2 ] ] + ' ' + 'hundred' + ' and ' + tenthDigit[ numberString[ 3 ] ] + '-' + digitToNineteen[ numberString[ 4 ] ]
+    } else {
+      return tenthDigit[ numberString[ 0 ] ] + '-' + digitToNineteen[ numberString[ 1 ] ] + ' ' + 'thousand' + ' ' + digitToNineteen[ numberString[ 2 ] ] + ' ' + 'hundred' + ' and ' + tenthDigit[ numberString[ 3 ] ] + '-' + digitToNineteen[ numberString[ 4 ] ];
+    }
+
+  }
  
 }
 console.log(numToWord(23678))
